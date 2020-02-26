d3bd225d9418d9cdb7011ff175fe690c20ffe29f
WRoenninger <49239695+WRoenninger@users.noreply.github.com>
2020-02-26 09:42:15 +0100

    fixup! tb_axi_to_axi_lite: Adapt TB to new module

--- test/tb_axi_to_axi_lite.sv
+++ test/tb_axi_to_axi_lite.sv
@@ -8,7 +8,9 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 //
-// Fabian Schuiki <fschuiki@iis.ee.ethz.ch>
+// Fabian Schuiki     <fschuiki@iis.ee.ethz.ch>
+// Andreas Kurth      <akurth@iis.ee.ethz.ch>
+// Wolfgang Ronninger <wroennin@iis.ee.ethz.ch>
 
 `include "axi/assign.svh"
 
